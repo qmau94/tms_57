18a3f0823a06c4e6a79994437a596c511c918e23
leduykhanhhedspi <leduykhanh.hedspi@gmail.com>
2016-06-20 17:37:57 +0700

    login logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
-  has_many :active_relationships, class_name: Relationship.name
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  attr_accessor :remember_token
+  
+  has_many :active_relationships, class_name: Relationship.name,
     foreign_key: "follower_id", dependent:   :destroy
-  has_many :passive_relationships, class_name: Relationship.name
+  has_many :passive_relationships, class_name: Relationship.name,
     foreign_key: "followed_id",
     dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
@@ -13,4 +17,41 @@ class User < ActiveRecord::Base
   has_many :courses, through: :user_courses
   has_many :user_subjects, dependent: :destroy
   has_many :subjects, through: :user_subjects
+
+  validates :name,  presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 1}
+
+  has_secure_password
+  
+  before_save {self.email = email.downcase}
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token 
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -105,8 +105,9 @@ ActiveRecord::Schema.define(version: 20160620084037) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.boolean  "role"
+    t.integer  "role"
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
