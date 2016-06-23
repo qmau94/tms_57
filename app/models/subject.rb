322e75189c487142094cac7d5cf4193d073795a3
qmau94 <qmau94@users.noreply.github.com>
2016-06-23 16:29:12 +0700

    create subjects

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,13 @@ class Subject < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :user_subjects, dependent: :destroy
   has_many :users, through: :user_subjects
+
+  validates :name, presence: true
+  validates :description, presence: true
+  accepts_nested_attributes_for :tasks, allow_destroy: true,
+    reject_if: :reject_tasks
+
+  def reject_tasks attributes
+    attributes["name"].blank? || attributes["description"].blank?
+  end
 end
